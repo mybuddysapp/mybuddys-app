1205cc498240a7e7780e238fb06d8ce7ab3a9199
puvistyne <56639092+Puvistyne-tech@users.noreply.github.com>
2024-04-18 01:13:12 +0200

    Revert "getx"

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:auth_provider/auth_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mybuddys/resources/pages/event/events.dart';
-import 'package:mybuddys/resources/pages/home/home_page.dart';
-import 'package:mybuddys/resources/widgets/landing_page.dart';
 import 'package:mybuddys/routes/route_config.dart';
 import 'package:mybuddys/utils/consts.dart';
+import 'package:nylo_support/nylo.dart';
 
-import 'app/translations/my_translations.dart';
 import 'bootstrap/boot.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Nylo nylo = await Nylo.init(setup: Boot.nylo, setupFinished: Boot.finished);
+  Nylo nylo = await Nylo.init(setup: Boot.nylo, setupFinished: Boot.finished);
 
   // initialize the user repository
   await SupabaseAuthProvider.initialize(
@@ -36,36 +32,14 @@ class MyApp extends StatelessWidget {
     return Consumer(
       builder: (_, ref, __) {
         final router = ref.watch(routerProvider);
-        return GetMaterialApp(
+        return MaterialApp.router(
           title: 'MyBuddys',
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
+          routerConfig: router,
           debugShowCheckedModeBanner: false,
-          home: LandingPage(),
-          themeMode: ThemeMode.system,
-          translations: MyTranslations(),
-          // your translations
-          locale: Locale('fr', 'FR'),
-          // translations will be displayed in that locale
-          fallbackLocale: Locale('fr', 'FR'), // specify the fa
         );
-        // return GetMaterialApp.router(
-        //   title: 'MyBuddys',
-        //   theme: ThemeData(
-        //     primarySwatch: Colors.blue,
-        //   ),
-        //   routeInformationProvider: router.routeInformationProvider,
-        //   routerDelegate: router.routerDelegate,
-        //   routeInformationParser: router.routeInformationParser,
-        //   debugShowCheckedModeBanner: false,
-        //   themeMode: ThemeMode.system,
-        //
-        //   translations: MyTranslations(), // your translations
-        //   locale: Locale('fr', 'FR'), // translations will be displayed in that locale
-        //   fallbackLocale: Locale('fr', 'FR'), // specify the fallback locale in case an invalid locale is selected.
-        //
-        // );
       },
     );
   }
